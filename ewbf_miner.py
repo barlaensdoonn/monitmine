46f3ddfcc22ced477fc645e3a83850cfd66f4a10
barlaensdoonn <brandon.aleson@gmail.com>
2017-06-24 11:56:40 -0700

    add retries to get_stats(); make print_stats()

--- ewbf_miner.py
+++ ewbf_miner.py
@@ -3,6 +3,7 @@
 # 6/22/17
 
 # temp, gpu_power_usage, speed_sps updated by api every 30 seconds
+# to properly average values this script should be running when miner starts
 
 import sys
 import time
@@ -12,27 +13,38 @@ from datetime import datetime
 
 
 class Miner(object):
-    'interact with the miner api'
+    '''interact with the miner api'''
 
     miner_url = minor.miner_url
 
     def __init__(self):
         self.polls = 0
-        self.sps = 0
-        self.average_sps = 0
         self.stats = self._get_stats()
         self.start_time = datetime.fromtimestamp(self._get_start_time())
         self.shares = {
             'total': {i: 0 for i in range(len(self.stats['result']))},
             'average': {i: 0 for i in range(len(self.stats['result']))}
         }
+        self.sps = {
+            'total': {i: 0 for i in range(len(self.stats['result']))},
+            'average': {i: 0 for i in range(len(self.stats['result']))},
+            'session_total': 0,
+            'session_average': 0
+        }
 
     def _get_stats(self):
-        try:
-            r = requests.get(self.miner_url)
-            return r.json()
-        except ConnectionError:
-            print('could not connect to the miner')
+        retries = 5
+
+        while retries:
+            try:
+                r = requests.get(self.miner_url)
+                return r.json()
+            except ConnectionError:
+                print('could not connect to miner\n')
+                retries -= 1
+
+        print('connect retries exhausted, exiting...')
+        sys.exit()
 
     def _get_start_time(self):
         return self.stats['result'][0]['start_time']
@@ -41,17 +53,31 @@ class Miner(object):
         self.time_up = datetime.now() - self.start_time
 
     def _get_sps(self):
-        for gpu in self.stats['result']:
-            self.sps += gpu['speed_sps']
+        for i in range(len(self.stats['result'])):
+            current_sps = self.stats['result'][i]['speed_sps']
+            self.sps['total'][i] += current_sps
+            self.sps['average'][i] = int(self.sps['total'][i] / self.polls)
+            self.sps['session_total'] += current_sps
 
-        self.average_sps = int(self.sps / self.polls)
+        self.sps['session_average'] = int(self.sps['session_total'] / self.polls)
 
     def _get_shares(self):
-        'to properly average shares over time this calculation should be running when miner starts'
-
         for i in range(len(self.stats['result'])):
             self.shares['total'][i] = self.stats['result'][i]['accepted_shares']
             self.shares['average'][i] = int(self.shares['total'][i] / self.polls)
+
+    def _print_stats(self):
+        print('time up: {}'.format(self.time_up))
+
+        for gpu in self.sps['average']:
+            print('average sps for gpu{}: {}'.format(gpu, self.sps['average'][gpu]))
+
+        print('session average sps: {} over {} polls'.format(self.sps['session_average'], self.polls))
+
+        for gpu in miner.shares['total']:
+            print('total shares for gpu{}: {}'.format(gpu, self.shares['total'][gpu]))
+
+        print('\n')
 
     def poll(self):
         self.stats = self._get_stats()
@@ -59,6 +85,7 @@ class Miner(object):
         self._get_time_up()
         self._get_sps()
         self._get_shares()
+        self._print_stats()
 
 
 if __name__ == '__main__':
@@ -70,16 +97,6 @@ if __name__ == '__main__':
             if int(datetime.now().timestamp() % 30) == 0:
                 miner.poll()
                 time.sleep(1)
-
-                print('time up: {}'.format(miner.time_up))
-                # print('total sps: {}'.format(miner.sps))
-                print('average_sps: {}'.format(miner.average_sps, miner.polls))
-
-                for key in miner.shares:
-                    for gpu in miner.shares[key]:
-                        print('{} shares for gpu{}: {}'.format(key, gpu, miner.shares[key][gpu]))
-
-                print('\n')
 
     except KeyboardInterrupt:
         print('\nexiting...')
