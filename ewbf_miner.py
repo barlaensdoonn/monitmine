25562951dc2486b4401cee9e2723f8d5063ddebd
barlaensdoonn <brandon.aleson@gmail.com>
2017-06-25 01:09:15 -0700

    remove __main__ section to use ewbf_miner.py more as module

--- ewbf_miner.py
+++ ewbf_miner.py
@@ -146,22 +146,3 @@ class Miner(object):
         self._get_up_time()
         self._update_stats()
         self._print_stats()
-
-
-if __name__ == '__main__':
-    miner = Miner()
-    polling = True
-
-    zec = coin.Coin('zec')
-    miner.inital_balance = zec.get_balance()
-    miner.last_payment = zec.get_last_payment()
-
-    try:
-        while polling:
-            if int(datetime.now().timestamp() % 30) == 0:
-                miner.poll()
-                time.sleep(1)
-
-    except KeyboardInterrupt:
-        print('\nexiting...')
-        polling = False
