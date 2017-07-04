17cd454dcf915166b9620e2589e66ebcc0164dcd
barlaensdoonn <brandon.aleson@gmail.com>
2017-07-03 20:23:34 -0700

    get stats from claymore miner api using sockets

---
+++ claymore_miner.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+# claymore miner api monitor
+# 7/03/17
+# updated 7/03/17
+
+import json
+import socket
+import minor
+
+host = minor.claymore_host
+port = minor.claymore_port
+
+
+def create_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    return client
+
+
+def create_request():
+    request = {'method': 'miner_getstat1', 'jsonrpc': '2.0', 'id': 0}
+
+    return json.dumps(request)
+
+
+def get_stats(client, request):
+    client.send(request.encode('ascii'))
+    response = client.recv(1024)
+
+    return json.loads(response.decode('ascii'))
+
+
+stats = get_stats(create_client(), create_request())
+print(stats)
