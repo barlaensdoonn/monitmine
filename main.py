03100ab52f787753da4968515e3c91cbf6cbc420
barlaensdoonn <brandon.aleson@gmail.com>
2017-07-09 15:06:57 -0700

    set up basic logging framework

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 #!/usr/local/bin/python3
 # mining monitor
 # 6/24/17
-# updated 6/26/17
+# updated 7/09/17
 
-
-import coin
-import ewbf_miner
-import earnings
+import yaml
 import time
+import logging
+import logging.config
 from datetime import datetime
+import coin
+import minor
+import earnings
+import ewbf_miner
+import claymore_miner
 
 
 if __name__ == '__main__':
-    miner = ewbf_miner.Miner()
+    with open(minor.log_conf, 'r') as log_conf:
+        log_config = yaml.safe_load(log_conf)
+
+    logging.config.dictConfig(log_config)
+
+    claymore = claymore_miner.Miner()
+    ewbf = ewbf_miner.Miner()
     zec = coin.Coin('zec')
-    earnings = earnings.Earnings(zec, miner)
+    earnings = earnings.Earnings(zec, ewbf)
 
     polling = True
 
     try:
         while polling:
             if int(datetime.now().timestamp() % 30) == 0:
-                miner.poll()
+                ewbf.poll()
                 earnings.update()
                 time.sleep(1)
 
